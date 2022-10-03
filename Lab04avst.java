46a590120ca6df3382310414785406a54d61ea14
15283 <jared.flores@vtsd.com>
2022-10-03 10:45:34 -0400

    jared f done

--- Lab04avst.java
+++ Lab04avst.java
@@ -9,19 +9,27 @@ public class Lab04avst
     {
         System.out.println("Lab04a, Student Version\n");
 
-        double principal  = 250000;
-        double annualRate = 4.85;
-        double numYears   = 30;
+        double p  = 250000;
+        double r = 4.85;
+        double ny   = 30;
+        double monthlyRate = (r/100)/12;
+        double numMonths = (ny*12);
+        double numerator = (monthlyRate * (Math.pow (monthlyRate + 1, numMonths)));
+        double denominator = (Math.pow (monthlyRate + 1, numMonths)) - 1;
+        double monthlyPayment = (numerator/denominator)*p;
+        double totalPayment = (monthlyPayment*numMonths);
+        double totalInterest = (totalPayment-p);
 
 
 
+        System.out.println("Principle:          $"+ p);
+        System.out.println("annualRate:         " + r+"%");
+        System.out.println("number of years:    $" + ny);
+        System.out.println("Monthly Payment:    $" + monthlyPayment);
+        System.out.println("Total Payments:    $"+ totalPayment);
+        System.out.println("Total interest:    $"+totalInterest);
 
 
-
-
-
-
-        System.out.println();
     }
 }
 
